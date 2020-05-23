55559b0234700d89b1066b6b839d213e633893aa
nardos <32473919+nardost@users.noreply.github.com>
2020-05-22 20:18:57 -0500

    exception package renamed to error

--- src/main/java/edu/depaul/ntessema/jaxrs/error/NotFoundExceptionMapper.java
+++ src/main/java/edu/depaul/ntessema/jaxrs/error/NotFoundExceptionMapper.java
@@ -1,21 +1,22 @@
-package edu.depaul.ntessema.jaxrs.exception;
+package edu.depaul.ntessema.jaxrs.error;
 
 import edu.depaul.ntessema.jaxrs.data.model.StatusMessage;
 
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class UnspecifiedExceptionMapper implements ExceptionMapper<Throwable> {
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
 
     @Override
-    public Response toResponse(Throwable throwable) {
+    public Response toResponse(NotFoundException nfe) {
         StatusMessage e = new StatusMessage(
-                Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),
-                "Unspecified error occurred");
+                Response.Status.NOT_FOUND.getStatusCode(),
+                "Endpoint not available.");
         return Response
-                .status(Response.Status.INTERNAL_SERVER_ERROR)
+                .status(Response.Status.NOT_FOUND)
                 .header("Content-Type", "application/json")
                 .entity(e)
                 .build();
--- src/main/java/edu/depaul/ntessema/jaxrs/error/UnspecifiedExceptionMapper.java
+++ src/main/java/edu/depaul/ntessema/jaxrs/error/UnspecifiedExceptionMapper.java
@@ -1,22 +1,21 @@
-package edu.depaul.ntessema.jaxrs.exception;
+package edu.depaul.ntessema.jaxrs.error;
 
 import edu.depaul.ntessema.jaxrs.data.model.StatusMessage;
 
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+public class UnspecifiedExceptionMapper implements ExceptionMapper<Throwable> {
 
     @Override
-    public Response toResponse(NotFoundException nfe) {
+    public Response toResponse(Throwable throwable) {
         StatusMessage e = new StatusMessage(
-                Response.Status.NOT_FOUND.getStatusCode(),
-                "Endpoint not available.");
+                Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),
+                "Unspecified error occurred");
         return Response
-                .status(Response.Status.NOT_FOUND)
+                .status(Response.Status.INTERNAL_SERVER_ERROR)
                 .header("Content-Type", "application/json")
                 .entity(e)
                 .build();
